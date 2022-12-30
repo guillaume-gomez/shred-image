9e582523383675970ee2556fb511773d77b4bec8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-12-30 13:26:42 +0100

    add animation on position for the stripes

--- src/Components/ThreeJsStripe.tsx
+++ src/Components/ThreeJsStripe.tsx
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import React, { useRef, useState } from 'react';
+import { useSpring, animated } from '@react-spring/three';
+import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame, ThreeElements, useLoader, useThree } from '@react-three/fiber';
 
 interface ThreeJsStripeProps {
@@ -12,9 +13,17 @@ interface ThreeJsStripeProps {
 
 
 function ThreeJsStripe({meshProps, base64Texture, widthStripe, heightStripe}: ThreeJsStripeProps) {
-  const mesh = useRef<THREE.Mesh>(null!);
   const { size: { width, height } } = useThree();
+  const [{ position }, api] = useSpring<any>(() =>({
+    from: meshProps.position,
+    position: meshProps.position,
+    config: { mass: 0.5, tension: 500, friction: 150, precision: 0.0001 }
+  }))
+  useEffect(() => {
+    api.start({ to: {position: meshProps.position}})
+  }, [meshProps, api])
  
+  const mesh = useRef<THREE.Mesh>(null!);
   //useFrame((state, delta) => (mesh.current.rotation.x += delta));
   const [texture] = useLoader(TextureLoader, [
     base64Texture
@@ -26,13 +35,14 @@ function ThreeJsStripe({meshProps, base64Texture, widthStripe, heightStripe}: Th
   }
 
   return (
-    <mesh
-      {...meshProps}
+    <animated.mesh
+      position={position as any}
       ref={mesh}
+      /*{...meshProps}*/
     >
       <boxGeometry args={[widthStripe, heightStripe, 0.1]} />
       <meshStandardMaterial map={texture} /*color={hovered ? 'hotpink' : 'orange'}*/ />
-    </mesh>
+    </animated.mesh>
   )
 }
 
